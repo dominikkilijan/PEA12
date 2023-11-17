9c9e856492b4c29f8b577527969f4f1e08374b59
dominikkilijan <dominikkilijan@gmail.com>
2023-11-17 07:37:37 +0100

    Cośtam dodane ale nie działa

--- Dynamic.cpp
+++ Dynamic.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <Windows.h>
 #include <time.h>
+#include <vector>
 
 using namespace std;
 
@@ -12,10 +13,62 @@ Dynamic::Dynamic(int n, int** m)
 {
 	N = n;
 	matrix = m;
+	visitedAll = (1 << N) - 1;
+	//vector<int> currentPath;
+	currentPath.reserve(N);
+
+	//vector<int> bestPath;
+	bestPath.reserve(N);
+	N2 = (1 << N);
+	dp = new int* [N2];
+	for (int i = 0; i < N2; i++)
+	{
+		dp[i] = new int[N];
+		for (int j = 0; j < N; j++)
+		{
+			dp[i][j] = -1;
+		}
+	}
+	
+}
+//------------------------------------------------------------------------------------------------------------------------------------
+int Dynamic::tsp(int mask, int pos)
+{
+	int currentSum;
+
+	if (mask == visitedAll) // jesli odwiedzono wszystkie wierzcholki
+		return matrix[pos][0];
+
+	if (dp[mask][pos] != -1) // ten przypadek juz byl liczony
+		return dp[mask][pos];
+
+	int bestSum = INT_MAX;
+	for (int i = 0; i < N; i++)
+	{
+		if ((mask & (1 << i)) == 0) // sprawdzenie czy wierzochlek byl juz odwiedzony
+		{
+			//currentPath.emplace_back(i);
+			currentSum = matrix[pos][i] + tsp(mask|(1<<i), i); // odleglosc z obecnego punktu do nastepnego + kolejne odcinki
+			bestSum = min(bestSum, currentSum);
+		}
+	}
+	/*if (bestSum > currentSum)
+	{
+		bestSum = currentSum;
+		bestPath.clear();
+		for (int i = 0; i < currentPath.size(); i++)
+		{
+			bestPath.emplace_back(currentPath[i]);
+		}
+		currentPath.clear();
+	}*/
+
+	return dp[mask][pos] = bestSum;
 }
 //------------------------------------------------------------------------------------------------------------------------------------
 long double Dynamic::TSPDynamic()
 {
+	
 	long long int frequency, start, elapsed; // zmienne zwiazane z pomiarem czasu
 	QueryPerformanceFrequency((LARGE_INTEGER*)&frequency);
 
@@ -23,7 +76,7 @@ long double Dynamic::TSPDynamic()
 	start = read_QPC(); 
 
 	// algorytm
-	Sleep(1000);
+	cout << tsp(1, 0) << "\n";
 	// koniec algorytmu
 
 	// koniec pomiaru czasu
@@ -32,10 +85,32 @@ long double Dynamic::TSPDynamic()
 	timeElapsed = ((1000.0 * elapsed) / frequency);
 
 	// wyswietlenie wyniku
-	cout << "Miejsce na wynik\n";
+	/*cout << "Miejsce na wynik\n";
+	for (int i = 0; i < bestPath.size(); i++)
+	{
+		cout << bestPath[i] << " ";
+	}
+	cout << endl;*/
+
+	cout << "dp:\n";
+	for (int i = 0; i < N2; i++)
+	{
+		dp[i] = new int[N];
+		for (int j = 0; j < N; j++)
+		{
+			cout << dp[i][j] << " ";
+		}
+		cout << endl;
+	}
 
 	// czyszczenie
 	cout << "Zwalnianie pamieci\n";
+	for (int i = 0; i < N2; i++)
+	{
+		delete[] dp[i];
+	}
+	delete[] dp;
+	dp = nullptr;
 
 	return timeElapsed;
 }
--- Dynamic.h
+++ Dynamic.h
@@ -1,19 +1,26 @@
 #pragma once
 #include "AdjacencyMatrix.h"
+#include <vector>
 
 class Dynamic
 {
 	public:
 		Dynamic(int, int**);
+		int tsp(int, int);
 		long long int read_QPC();
 		long double TSPDynamic();
 
 	private:
-		int* bestPath;
-		int bestSum = 2147483647; // najwieksza dostepna wartosc
-		int* currentPath;
+		//int* bestPath;
+		//int bestSum = 2147483647; // najwieksza dostepna wartosc
+		//int* currentPath;
+		vector<int> currentPath;
+		vector<int> bestPath;
 
+		int** dp;
 		int N;
+		int N2;
 		int** matrix;
+		int visitedAll;
 };
 
